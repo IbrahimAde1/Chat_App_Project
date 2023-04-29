707a9c42283535775083c8fdb3918cca9a0117d6
IbrahimAde1 <ibrahim.adel10110@gmail.com>
2023-04-29 17:11:52 +0300

    first version

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,9 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: Login(),
+      home: const Login(),
     );
   }
 }
